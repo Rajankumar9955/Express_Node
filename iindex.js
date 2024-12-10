8a54317c7734f5af5178ed2ee82cd28006f38065
Rajan <krajan92946@gmail.com>
2024-12-10 19:29:54 +0530

    get request

---
+++ iindex.js
@@ -0,0 +1,25 @@
+
+
+
+const express = require("express");
+
+const app=express();
+const port=8080;
+
+app.get("/", (req,res)=>{
+    res.send("THis is first app");
+ })
+  app.get("/about", (req,res)=>{
+    res.send("this is About");
+  })
+  app.get("/gallery", (req,res)=>{
+    res.send("this is gallery");
+  })
+  app.get("/photo/images", (req,res)=>{
+    res.send("this is IMages");
+  })
+
+app.listen(port,()=>{
+    console.log(`This is ${port}`)
+})
+

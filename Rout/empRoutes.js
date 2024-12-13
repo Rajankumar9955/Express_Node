789a9284cfdd6ccfd03dfaba607ed899f006a045
Rajan <krajan92946@gmail.com>
2024-12-13 19:32:18 +0530

    2nd part on mcv controller end

--- Rout/empRoutes.js
+++ Rout/empRoutes.js
@@ -3,6 +3,7 @@ const express=require("express");
 const route=express.Router();
 const EmpControlller=require("../Controller/empController")
 
+
 route.get("/",EmpControlller.empHOme)
 route.get("/about",EmpControlller.empAbout);
 route.get("/contact",EmpControlller.empContact)
